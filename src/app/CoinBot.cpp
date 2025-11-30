24038cd2e25d6489fba610a95b1d4eacedf57b2d
outguk <dlsrnr454@gmail.com>
2025-11-30 18:22:12 +0900

    Add project files.

---
+++ src/app/CoinBot.cpp
@@ -0,0 +1,12 @@
+﻿// CoinBot.cpp : Defines the entry point for the application.
+//
+
+#include "../CoinBot.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
